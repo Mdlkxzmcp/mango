fcbd0fd87a8c2f3aec9099d117387a7cc67dc678
Maximiliaan Strother III <maxbax@op.pl>
2018-04-11 17:24:29 +0200

    Initial commit pt. 3 (final)
    Wouldn't call this an initial in a normal project, but since all the files were provided here, I chose to call it so.  This also in a sense follows removing boilerplate, so there's that...

--- test/mango_web/controllers/page_controller_test.exs
+++ test/mango_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule MangoWeb.PageControllerTest do
   use MangoWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Welcome to Mango!"
   end
 end
